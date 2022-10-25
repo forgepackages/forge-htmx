aa7ce637f83b5d6b24c9954262a09fb3926f6636
Dave Gaeddert <dave.gaeddert@gmail.com>
2022-10-24 21:31:56 -0500

    Fix flaky defineExtension for error-classes

--- forgehtmx/static/htmx/forgehtmx.js
+++ forgehtmx/static/htmx/forgehtmx.js
@@ -23,28 +23,41 @@ window.addEventListener("load", function () {
   });
 });
 
-htmx.defineExtension("error-classes", {
-  onEvent: function (name, evt) {
-    if (name === "htmx:beforeRequest") {
-      // TODO use the value from hx-indicator
-      var target = evt.detail.target;
-      // Remove every class that starts with htmx-error
-      for (var i = 0; i < target.classList.length; i++) {
-        if (target.classList[i].startsWith("htmx-error-")) {
-          target.classList.remove(target.classList[i]);
+var extensionDefined = false;
+function defineExtension() {
+  htmx.defineExtension("error-classes", {
+    onEvent: function (name, evt) {
+      if (name === "htmx:beforeRequest") {
+        // TODO use the value from hx-indicator
+        var target = evt.detail.target;
+        // Remove every class that starts with htmx-error
+        for (var i = 0; i < target.classList.length; i++) {
+          if (target.classList[i].startsWith("htmx-error-")) {
+            target.classList.remove(target.classList[i]);
+          }
         }
       }
-    }
 
-    if (name === "htmx:responseError") {
-      var target = evt.detail.target;
-      htmx.addClass(target, "htmx-error-response");
-      htmx.addClass(target, "htmx-error-response-" + evt.detail.xhr.status);
-    }
+      if (name === "htmx:responseError") {
+        var target = evt.detail.target;
+        htmx.addClass(target, "htmx-error-response");
+        htmx.addClass(target, "htmx-error-response-" + evt.detail.xhr.status);
+      }
 
-    if (name === "htmx:sendError") {
-      var target = evt.detail.target;
-      htmx.addClass(target, "htmx-error-send");
-    }
-  },
-});
+      if (name === "htmx:sendError") {
+        var target = evt.detail.target;
+        htmx.addClass(target, "htmx-error-send");
+      }
+    },
+  });
+  extensionDefined = true;
+}
+
+if (typeof htmx !== "undefined") {
+  defineExtension();
+} else {
+  window.addEventListener("htmx:load", function() {
+    if (extensionDefined) return;
+    defineExtension();
+  });
+}
